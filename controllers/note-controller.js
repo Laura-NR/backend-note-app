dd6dcab7cd1d9af7c0a28b72c4ccb87046b257ba
GENYST\laura <nunez.laura.e@protonmail.com>
2024-02-23 16:31:34 +0100

    Intent to add category

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -3,42 +3,31 @@ import mysql from 'mysql2/promise';
 export class NoteController {
   async listAll(req, res) {
     console.log('noteController should list them all');
-    const dbConnection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'note'
-    });
-    console.log('connexion db réussie');
+    const dbConnection = await this.createDBConnection();
     const [results, fields] = await dbConnection.query('SELECT * FROM note');
     res.send(results);
   }
 
-  //Create new note
   async create(req, res) {
     console.log('noteController create');
-    const newNote = req.body;
-    
-    const dbConnection = await mysql.createConnection({
-      host: 'localhost',
-      user: 'root',
-      database: 'note'
-    });
-
-    const sql = 'INSERT INTO note (text) VALUE (?)';
-    const [results, fields] = await dbConnection.query(sql, [req.body.text]);
-
-    res.json({message: "note added to database"});
+    const dbConnection = await this.createDBConnection();
+    const sql = 'INSERT INTO note (text, category) VALUES (?, ?)';
+    const [results, fields] = await dbConnection.query(sql, [req.body.text, req.body.category]); // Pass both text and category to the query
+    res.json({ message: "note added to database" });
   }
 
-  //Delete note
   async destroy(req, res) {
-    const dbConnection = await mysql.createConnection({
+    const dbConnection = await this.createDBConnection();
+    console.log('connexion db réussie');
+    const [results, fields] = await dbConnection.query('DELETE FROM note WHERE id = ?', [req.params.id]);
+    res.json({message: "note deleted", results: results});
+  }
+
+  async createDBConnection() {
+    return mysql.createConnection({
       host: 'localhost',
       user: 'root',
       database: 'note'
     });
-    console.log('connexion db réussie');
-    const [results, fields] = await dbConnection.query('DELETE FROM note WHERE id = ?', [req.params.id])
-    res.json({message: "note deleted", results: results})
   }
 }
