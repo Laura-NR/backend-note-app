30bbdb8499657ac47e57367dd13af11da2bf5de4
GENYST\laura <nunez.laura.e@protonmail.com>
2024-02-26 22:19:16 +0100

    Counter fixed and form corrected

--- controllers/note-controller.js
+++ controllers/note-controller.js
@@ -21,7 +21,26 @@ export class NoteController {
     const dbConnection = await this.createDBConnection();
     console.log('connexion db réussie');
     const [results, fields] = await dbConnection.query('DELETE FROM note WHERE id = ?', [req.params.id]);
-    res.json({message: "note deleted", results: results});
+    res.json({ message: "note deleted", results: results });
+  }
+
+  async update(req, res) {
+    console.log("Update function");
+    console.log(req);
+    const dbConnection = await this.createDBConnection();
+    const sql = 'UPDATE note SET text = ?, category = ? WHERE id = ?';
+    const [results, fields] = await dbConnection.query(sql, [req.body.text, req.body.category, req.params.id]);
+    console.log(results);
+    console.log(fields);
+    // Check if any rows were affected by the update
+    if (results.affectedRows === 1) {
+      // Fetch the updated note from the database
+      const [updatedNote] = await dbConnection.query('SELECT * FROM note WHERE id = ?', [req.params.id]);
+      // Send back the updated note in the response
+      res.json({ message: "note updated", note: updatedNote });
+    } else {
+      res.status(404).json({ message: "note not found" });
+    }
   }
 
   async createDBConnection() {
